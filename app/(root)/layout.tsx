ef4bf47df03497d3a4f543a60a644a90a7a0cbc8
Ali-Traboulsi <alitraboulsi111@gmail.com>
2024-02-18 17:00:59 +0200

    added-sidebar-component-with-auth-to-root-group

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,11 @@
+import Sidebar from "@/components/shared/sidebar";
 import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="auth">
+    <main className="root">
+      <Sidebar />
+      {/* -- <MobileNav /> */}
       <div className="root-container">
         <div className="wrapper">{children}</div>
       </div>
